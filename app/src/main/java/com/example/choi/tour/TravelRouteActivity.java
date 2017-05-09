835dc4109518e46cf5c37d6334881a0c5d3ca31d
Hyeonwoo-Choi <hw.choi@kaist.ac.kr>
2017-05-09 17:25:30 +0900

    Add travel route (activity, layout, gradle)

--- app/src/main/java/com/example/choi/tour/TravelRouteActivity.java
+++ app/src/main/java/com/example/choi/tour/TravelRouteActivity.java
@@ -1,17 +1,45 @@
 package com.example.choi.tour;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by choi on 2017-04-12.
  */
 
-public class TravelRouteActivity extends AppCompatActivity{
+public class TravelRouteActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
-    protected void onCreate(Bundle bundle){
-        super.onCreate(bundle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.travel_route);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        MapFragment mapFragment = (MapFragment)fragmentManager
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        map.addMarker(markerOptions);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
